036cc6457a59d1d6df559afeeecd0159674ccdb6
Kostek <ksytnik73kwer@gmail.com>
2024-01-27 23:05:09 +0100

    Add user model migration and update database configuration.

--- backend/infrastructure/migrations/env.py
+++ backend/infrastructure/migrations/env.py
@@ -4,8 +4,12 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
+from infrastructure.database.models import Base
 
 from alembic import context
+
+from config import DbConfig
+from environs import Env
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,13 +24,21 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+env = Env()
+env.read_env()
+db_config = DbConfig.from_env(env)
+
+config.set_main_option(
+    "sqlalchemy.url",
+    db_config.construct_sqlalchemy_url(),
+)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
