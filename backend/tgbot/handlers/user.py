2a9cb3beff13aa741b9e78e905e0437c47608a51
Kostek <ksytnik73kwer@gmail.com>
2024-01-30 23:02:20 +0100

    Add miniapp keyboard and update user_start handler.

--- backend/tgbot/handlers/user.py
+++ backend/tgbot/handlers/user.py
@@ -2,9 +2,14 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from tgbot.keyboards.miniapp import get_miniapp_link
+from config import Config
+
 user_router = Router()
 
 
 @user_router.message(CommandStart())
-async def user_start(message: Message):
-    await message.reply("Вітаю, звичайний користувач!")
+async def user_start(message: Message, config: Config):
+    await message.reply(
+        text="Greetings dear user!", reply_markup=get_miniapp_link(config.tg_bot.web_app_url)
+    )
