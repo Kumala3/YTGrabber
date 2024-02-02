615e207e6ae9dd6fcf984c2b6cde4ce760f0a248
Kostek <ksytnik73kwer@gmail.com>
2024-02-02 20:50:40 +0100

    Delete video handler. Update constants -> create start_text function. Update start_keyboard function and add back_keyboard & menu_keyboard. Add new handlers for processing menu and back_buttons.

--- backend/tgbot/handlers/user.py
+++ backend/tgbot/handlers/user.py
@@ -1,10 +1,10 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.enums import ParseMode
 
-from tgbot.keyboards.inline import start_user_keyboard
-from tgbot.misc.constants import START_TEXT
+from tgbot.keyboards.inline import start_keyboard, back_keyboard, menu_keyboard
+from tgbot.misc.constants import start_text
 from config import Config
 
 user_router = Router()
@@ -13,8 +13,65 @@ user_router = Router()
 @user_router.message(CommandStart())
 async def user_start(message: Message, config: Config):
     await message.reply(
-        text=START_TEXT,
-        reply_markup=start_user_keyboard(config.misc.web_app_url),
+        text=start_text(config.misc.repo_url),
+        reply_markup=start_keyboard(config.misc.web_app_url),
         parse_mode=ParseMode.HTML,
         disable_web_page_preview=True,
+    )
+
+
+@user_router.callback_query(F.data == "continue_in_bot")
+async def menu(query: CallbackQuery):
+    await query.message.edit_text(
+        text="Menu",
+        reply_markup=menu_keyboard(),
+    )
+
+
+@user_router.callback_query(F.data == "profile")
+async def get_profile(query: CallbackQuery):
+    await query.message.edit_text(
+        text="Profile",
+        reply_markup=back_keyboard("menu"),
+    )
+
+
+@user_router.callback_query(F.data == "download_video")
+async def get_video_link(query: CallbackQuery):
+    await query.message.edit_text(
+        text="Enter the link or video_id to the video:",
+        reply_markup=back_keyboard("menu"),
+    )
+
+
+@user_router.callback_query(F.data == "downloaded_videos")
+async def get_downloaded_videos(query: CallbackQuery):
+    await query.message.edit_text(
+        text="Here are your downloaded videos:",
+        reply_markup=back_keyboard("menu"),
+    )
+
+
+@user_router.callback_query(F.data == "support")
+async def get_support(query: CallbackQuery):
+    await query.message.edit_text(
+        text="Contact support:",
+        reply_markup=back_keyboard("menu"),
+    )
+
+
+@user_router.callback_query(F.data == "get_back_start")
+async def get_back(query: CallbackQuery, config: Config):
+    await query.message.edit_text(
+        text=start_text(config.misc.repo_url),
+        reply_markup=start_keyboard(config.misc.web_app_url),
+        disable_web_page_preview=True,
+    )
+
+
+@user_router.callback_query(F.data == "get_back_menu")
+async def back_to_menu(query: CallbackQuery):
+    await query.message.edit_text(
+        text="Menu",
+        reply_markup=menu_keyboard(),
     )
--- backend/tgbot/keyboards/inline.py
+++ backend/tgbot/keyboards/inline.py
@@ -1,16 +1,8 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import WebAppInfo
+from aiogram.types import WebAppInfo, InlineKeyboardMarkup
 
 
-def get_back_keyboard():
-    keyboard = InlineKeyboardBuilder()
-
-    keyboard.button(text="Get back", callback_data="get_back")
-
-    return keyboard.as_markup()
-
-
-def start_user_keyboard(url: str):
+def start_keyboard(url: str) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardBuilder()
 
     keyboard.button(
@@ -18,5 +10,27 @@ def start_user_keyboard(url: str):
         web_app=WebAppInfo(url=url),
     )
     keyboard.button(text="Continue in the bot ➡️", callback_data="continue_in_bot")
+
+    return keyboard.as_markup()
+
+
+def back_keyboard(url: str = None) -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardBuilder()
+
+    keyboard.button(text="⬅️ Back", callback_data=f"get_back_{url}")
+
+    return keyboard.as_markup()
+
+
+def menu_keyboard() -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardBuilder()
+
+    keyboard.button(text="👤 Profile", callback_data="profile")
+    keyboard.button(text="📞 Support", callback_data="support")
+    keyboard.button(text="📺 Download video", callback_data="download_video")
+    keyboard.button(text="📫 Downloaded_videos", callback_data="downloaded_videos")
+    keyboard.button(text="⬅️ Back", callback_data="get_back_start")
+
+    keyboard.adjust(2, 2, 1)
 
     return keyboard.as_markup()
